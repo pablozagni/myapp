1dbbc3690564f227cb0477232cccee000ee245cc
Pablo Zagni <pablozagniyahoo.com.ar>
2023-03-07 19:57:26 -0300

    01.

--- resources/views/components/inputs-select.blade.php
+++ resources/views/components/inputs-select.blade.php
@@ -11,7 +11,7 @@
             @if (isset($name)) name="{{ $name }}" @endif
             @if (isset($id)) id="{{ $id }}" @endif
             @if (isset($multiple)) multiple @endif
-            {{-- {{ $attributes->merge(['class'=>'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'])}} --}}
+            {{-- {{ $attributes->merge(['class'=>'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'])}} --}}
             {{ $attributes->merge(['class'=>'w-full form-control'])}}
             >
             
